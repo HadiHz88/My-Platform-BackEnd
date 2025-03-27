3d587c08d2ffce523dc8d1b91efdab4271a8f7fc
Hadi Hz <hijazih24@gmail.com>
2025-03-27 07:55:06 +0200

    Change the data parsed in /home api

--- app/Http/Controllers/Public/HomeController.php
+++ app/Http/Controllers/Public/HomeController.php
@@ -5,6 +5,7 @@ use App\Models\Course;
 use App\Models\Entry;
 use App\Models\Project;
 use App\Models\Tag;
+use Illuminate\Support\Facades\Storage;
 
 class HomeController extends \App\Http\Controllers\Controller
 {
@@ -22,12 +23,34 @@ class HomeController extends \App\Http\Controllers\Controller
         // latest course updated
         $latestCourses = Course::with(['tags'])->take(3)->get();
 
+        // logos
+        $logo_white_on_black= asset('storage/logos/logo_white_on_black.png');
+        $logo_black_on_white= asset('storage/logos/logo_white_on_black.png');
+        $logo_blue_on_black = asset('storage/logos/logo_blue_on_black.png');
+
+        // Profile info
+        $profileInfo = [
+            'name' => 'John Doe',
+            'title' => 'Full Stack Developer',
+            'bio' => '',
+            'github' => '',
+            'linkedin' => '',
+            'email' => '',
+            'resumeUrl' => '',
+            'profileImage' => url(asset('storage/logos/profile_image.jpg'))
+        ];
+
+
         return response()->json([
             'skills' => $skills,
             'educations' => $educations,
             'experiences' => $experiences,
             'latestProjects' => $latestProjects,
-            'latestCourses' => $latestCourses
+            'latestCourses' => $latestCourses,
+            'logo_white_on_black' => url($logo_white_on_black),
+            'logo_black_on_white' => url($logo_black_on_white),
+            'logo_blue_on_black' => url($logo_blue_on_black),
+            'profileInfo' => $profileInfo,
         ]);
     }
 }
